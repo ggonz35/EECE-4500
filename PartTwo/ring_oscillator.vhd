910be31253af0c43290a5413ade3aaad062305bd
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-10-16 20:41:22 -0400

    Update ring_oscillator.vhd

--- PartTwo/ring_oscillator.vhd
+++ PartTwo/ring_oscillator.vhd
@@ -12,28 +12,27 @@ entity ring_oscillator is
 end entity ring_oscillator;
 
 architecture gen of ring_oscillator is
-	signal result: std_logic_vector(0 to ro_length-1):= 0;
+	signal result: std_logic_vector(0 to ro_length-1);
+	signal hold: std_logic;
 	
 begin
-	assert <ro_length mod 2 = 1>
-		report "ro_length must be an odd number"
-		severity failure;
 
 	-- place nand gate
 	-- one input is enable, the other one the output of the last inverter
 	-- output goes into the first inverter in the chain
-		result(ro_length-1) <= enable nand osc_out;
+		osc_out <= hold;
+		result(0) <= enable nand hold;
 		
 		-- place inverters
 		-- for ... generate
 		-- end generate
-		gen_chain: for i in 0 to (ro_length - 1) generate
+		gen_chain: for i in 1 to (ro_length - 2) generate
 		
-				result(i+1) <= not result(i);
+				result(i) <= not result(i-1);
 				
 		end generate gen_chain;
 
 	-- drive osc_out with output of last inverter in the chain
-		osc_cout <= result(ro_length-1);
+		hold <= result(ro_length-1);
 		
 end architecture gen;

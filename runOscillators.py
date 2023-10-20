e37ed63be895de1ee9f0f117b707d641a6ba2e4f
Gabriel Gonzalez <104934278+ggonz35@users.noreply.github.com>
2023-10-20 19:55:18 -0400

    Update runOscillators.py

--- runOscillators.py
+++ runOscillators.py
@@ -5,12 +5,14 @@ def clamp(value, minimum, maximum):
     return max(minimum, min(value, maximum))
 
 # Parameters for process variations and accepted ranges
-tplv_nom = 1.0
-tpwv_nom = 1.0
-tnln_nom = 1.0
-tnwn_nom = 1.0
-tpotv_nom = 1.0
-tnotv_nom = 1.0
+param_nominal = {
+    "tplv": 1.0,
+    "tpwv": 1.0,
+    "tnln": 1.0,
+    "tnwn": 1.0,
+    "tpotv": 1.0,
+    "tnotv": 1.0
+}
 
 parameter_variations = {
     "tplv": (0.85, 1.15),
@@ -28,26 +30,29 @@ for i in range(num_oscillators):
     circuit_filename = f"ring_oscillator_{i}.cir"
     with open(circuit_filename, "w") as circuit_file:
         # Write the circuit description
-        circuit_file.write("* Ring Oscillator Subcircuit\n")
-        circuit_file.write(f".subckt ring_oscillator_{i} in out inverter\n")
+        circuit_file.write(f"* Ring Oscillator Subcircuit {i}\n")
 
         # Randomly vary parameters within accepted ranges
         for param, (min_value, max_value) in parameter_variations.items():
             variation = random.uniform(min_value, max_value)
-            parameter_value = clamp(tplv_nom * variation, min_value, max_value)
+            parameter_value = clamp(param_nominal[param] * variation, min_value, max_value)
             circuit_file.write(f".param {param} = {parameter_value}\n")
 
-        # Include the inverter and define the ring oscillator
+        # Include the subcircuits for the inverter and NAND gate
         circuit_file.write(".include inverter.txt\n")
         circuit_file.write(".include nand.txt\n")
-        # Define the ring oscillator with inverters
+
+        # Define the ring oscillator with 12 inverters
         for j in range(12):
             in_pin = "in1" if j == 0 else f"out{j}"
-            out_pin = f"out{j + 1}" if j < 11 else "out1"
-            circuit_file.write(f"X{j + 1} {in_pin} {out_pin} inverter L=tplv*130n W=tpwv*130n\n")
+            out_pin = f"out{j+1}" if j < 11 else "out1"
+            circuit_file.write(f"X{j+1} {in_pin} {out_pin} inverter L=tplv*130n W=tpwv*130n\n")
 
-            # Connect the NAND gate to the ring oscillator with an enable signal
-        circuit_file.write("X13 in1 in2 out1 nand\n")
-        circuit_file.write("X14 out1_nand out2_nand out1_nand nand\n")
+        # Define the enable signal
+        circuit_file.write("Venable enable 0 PULSE(0 1 0 1n 1n 0.01n 0.02n 100n) DC 0\n")
+
+        # Connect the NAND gate to the ring oscillator with an enable signal
+        circuit_file.write("X13 in1 enable out1_nand nand\n")
+        circuit_file.write("X14 out1_nand in2 out1_nand nand\n")
 
 print(f"{num_oscillators} ring oscillator subcircuits generated.")

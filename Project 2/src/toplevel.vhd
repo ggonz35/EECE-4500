33f6dd92fb03ee48c7868ac7d735a383bc2e0d90
Gabriel Gonzalez <104934278+ggonz35@users.noreply.github.com>
2023-11-08 18:08:12 -0500

    steve is love steve is life

--- Project 2/src/toplevel.vhd
+++ Project 2/src/toplevel.vhd
@@ -1,22 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 use std.textio.all;
 
-use work.ads.all;
-use work.vga.all;
+use work.ads_complex_pkg.all;
+use work.vga_data.all;
 
-entity mandlebrot is
-end mandlebrot;
+entity mandelbrot is
+end entity mandelbrot;
 
-architecture steve of mandlebrot is
+architecture steve of mandelbrot is
 
-    constant PATH : string  := "C:\Users\Zeph\Desktop\steve_one.bmp";
-    type STD_FILE is file of std_logic_vector(4- downto 0);
-    file fileptr : STD_FILE;
+    constant PATH: string := "C:\Users\Zeph\Desktop\steve_one.bmp";
+    type STD_FILE is file of std_logic_vector(3 downto 0); -- Change size to 4 bits
+    file fileptr: STD_FILE;
 
 begin
+
     -- Add your VGA configuration and color map setup here
     procedure generate_set is
         generic
@@ -26,26 +26,21 @@ begin
         variable c: ads_complex;
         variable color_index: natural;
         variable color: std_logic_vector(3 downto 0);
+        variable FILE_OPEN_STATUS: FILE_OPEN_STATUS; -- Declare the FILE_OPEN_STATUS variable
     begin
-
-    file_open(FILE_OPEN_STATUS, fileptr, PATH, write_mode);
+        file_open(FILE_OPEN_STATUS, fileptr, PATH, write_mode);
 
         for l in 0 to 479 loop
             for p in 0 to 639 loop
                 if x_visible(make_coordinate(p, l)) and y_visible(make_coordinate(p, l)) then
-
-                    c := ads_cmplx((3.2 * to_ads_sfixed(p) / 640.0 - 2.2), (2.2 * (240 - l) / 480.0));
+                    c := ads_cmplx(to_ads_sfixed(3.2 * real(p) / 640.0 - 2.2), to_ads_sfixed(2.2 * real(240 - l) / 480.0));
                     color_index := compute_point(c, iterations);
                     color := vga_color_map(color_index);
-
-                    writeline(fileptr, color(2) & string'(" ") & color(1) & string'(" ") & color(0) & string'(" "));
-
+                    writeline(fileptr, color(2) & color(1) & color(0) & "00"); -- Adjust for 4-bit color
                 end if;
             end loop;
         end loop;
-
-    file_close(fileptr);
-
+        file_close(fileptr);
     end procedure;
 
     function compute_point(c: in ads_complex; iterations: in positive) return natural is
@@ -62,7 +57,4 @@ begin
         end loop;
         return 0;
     end function compute_point;
-    end loop;
-end procedure;
-
-end steve;
+end architecture steve;

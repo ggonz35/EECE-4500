334107ea427bd4de478db0c577586dab6833700d
Gabriel Gonzalez <104934278+ggonz35@users.noreply.github.com>
2023-11-13 17:07:55 -0500

    The hat man is coming for your pancreas

--- Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
+++ Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
@@ -1,65 +1,65 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.ads.all;  -- Assuming ads_complex_pkg is in the work library
 
 entity generate_set is
-	port(
-	clk : in STD_LOGIC;
-       rst : in STD_LOGIC;
-       stage_input : in PipelineRecord;
-       stage_output : out PipelineRecord
-	);
-	
+    port (
+        clk : in STD_LOGIC;
+        rst : in STD_LOGIC;
+        stage_input : in PipelineRecord;
+        stage_output : out PipelineRecord
+    );
 end generate_set;
 
-
 architecture set_gen of generate_set is
-	-- signal decleration
-	signal z_real_part_temp, z_imaginary_part_temp : real;
-   signal z_overflow, c_overflow : boolean;
-	signal stage_data: natural;
-	signal stage_overflow: boolean;
--- Creates a type called color_map_array with a size of maximum iterations
-type color_map_array is array (0 to iterations) of ads_complex;
+    -- signal declaration
+    signal z_real_part_temp, z_imaginary_part_temp : real;
+    signal z_overflow, c_overflow : boolean;
+    signal stage_data: natural;
+    signal stage_overflow: boolean;
 
-begin
+    -- Creates a type called color_map_array with a size of maximum iterations
+    type color_map_array is array (natural range <>) of ads_complex;
 
-procedure compute_point
-    -- Procedures dont return values so we need to define our output as a variable with type "out"
-    (variable c: in ads_complex;
-    variable iterations: in natural
-    variable color_map: out color_map_array) is
-
-    generic(
-        threshold: positive := 64
+    generic (
+        threshold: positive := 64;
+        iterations: natural := 100;  -- Set an appropriate value
     );
 
-    variable z: ads_complex;
-    variable iteration: natural; -- This is never a negative number bc its an iteration
-begin
-    -- Initialize z and iteration
-     z := 0;
-     iteration := 0;
+    procedure compute_point
+        (variable c: in ads_complex;
+         variable iterations: in natural;
+         variable color_map: out color_map_array);
+    end procedure;
 
-     -- Loop iterations amount of times
-     while iteration < iterations loop
-                     --perform computations and complete assignments
-			-- Calculate stage_data and stage_overflow based on the rules
-			stage_output.stage_data <= stage_input.stage_data when stage_input.stage_overflow else stage_number;
-			stage_output.stage_overflow <= stage_input.stage_overflow or (z_overflow or c_overflow);
-			-- Assign c output as it is
-			stage_output.c <= stage_input.c;
-			-- Compute z^2 + c
-			z_real_part_temp <= (stage_input.z.real_part ** 2) - (stage_input.z.imaginary_part ** 2) + stage_input.c.real_part;
-			z_imaginary_part_temp <= (2.0 * stage_input.z.real_part * stage_input.z.imaginary_part) + stage_input.c.imaginary_part;
-			-- Calculate overflow flags for z and c
-			z_overflow <= (abs(z_real_part_temp) > threshold) or (abs(z_imaginary_part_temp) > threshold);
-			c_overflow <= (abs(stage_input.c.real_part) > threshold) or (abs(stage_input.c.imaginary_part) > threshold);
-			-- Use temporary values to update z
-			stage_output.z.real_part <= z_real_part_temp;
-			stage_output.z.imaginary_part <= z_imaginary_part_temp;
-			
-        iteration := iteration + 1;
-        color_map(iteration) := z;
-     end loop;
-	end procedure;
-end compute_point;
+    -- Define the PipelineRecord type
+    type PipelineRecord is record
+        z : ads_complex;
+        c : ads_complex;
+        stage_data : natural;
+        stage_overflow : boolean;
+    end record;
+
+begin
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            -- Reset logic if needed
+            -- ...
+        elsif rising_edge(clk) then
+            -- Your pipeline stage logic here
+
+            -- Initialize color_map_array
+            signal my_color_map : color_map_array(0 to iterations - 1) := (others => ads_complex_pkg.complex_zero);
+
+            -- Call the procedure with appropriate parameters
+            compute_point(stage_input.c, iterations, my_color_map);
+
+            -- Update stage_output based on your pipeline logic
+            stage_output.z <= ...;  -- Update based on your logic
+            stage_output.c <= ...;  -- Update based on your logic
+            stage_output.stage_data <= ...;  -- Update based on your logic
+            stage_output.stage_overflow <= ...;  -- Update based on your logic
+        end if;
+    end process;
+end set_gen;

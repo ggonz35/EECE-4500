b1631085a3d49f965e58360c7506ff4478295e90
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-11-13 16:57:05 -0500

    getting piped down by the pipeline

--- Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
+++ Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
@@ -2,13 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity generate_set is
-	port();
+	port(
+	clk : in STD_LOGIC;
+       rst : in STD_LOGIC;
+       stage_input : in PipelineRecord;
+       stage_output : out PipelineRecord
+	);
 	
 end generate_set;
 
 
 architecture set_gen of generate_set is
 	-- signal decleration
+	signal z_real_part_temp, z_imaginary_part_temp : real;
+   signal z_overflow, c_overflow : boolean;
+	signal stage_data: natural;
+	signal stage_overflow: boolean;
 -- Creates a type called color_map_array with a size of maximum iterations
 type color_map_array is array (0 to iterations) of ads_complex;
 
@@ -33,14 +42,24 @@ begin
 
      -- Loop iterations amount of times
      while iteration < iterations loop
-        z := ads_square(z) + c;
-
-            if abs2(z) > threshold then
-                exit; -- not sure if using exit is correct but we need to break here
-            end if;
-
+                     --perform computations and complete assignments
+			-- Calculate stage_data and stage_overflow based on the rules
+			stage_output.stage_data <= stage_input.stage_data when stage_input.stage_overflow else stage_number;
+			stage_output.stage_overflow <= stage_input.stage_overflow or (z_overflow or c_overflow);
+			-- Assign c output as it is
+			stage_output.c <= stage_input.c;
+			-- Compute z^2 + c
+			z_real_part_temp <= (stage_input.z.real_part ** 2) - (stage_input.z.imaginary_part ** 2) + stage_input.c.real_part;
+			z_imaginary_part_temp <= (2.0 * stage_input.z.real_part * stage_input.z.imaginary_part) + stage_input.c.imaginary_part;
+			-- Calculate overflow flags for z and c
+			z_overflow <= (abs(z_real_part_temp) > threshold) or (abs(z_imaginary_part_temp) > threshold);
+			c_overflow <= (abs(stage_input.c.real_part) > threshold) or (abs(stage_input.c.imaginary_part) > threshold);
+			-- Use temporary values to update z
+			stage_output.z.real_part <= z_real_part_temp;
+			stage_output.z.imaginary_part <= z_imaginary_part_temp;
+			
         iteration := iteration + 1;
         color_map(iteration) := z;
      end loop;
 	end procedure;
-end compute_point
+end compute_point;

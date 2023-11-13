9c7245c0c0a5e8b2b8e2e08e8d09bc3cf3c75101
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-11-13 18:11:55 -0500

    Update algorithm_two.vhd

--- Project 2/Algorithms_INCOMPLETE/algorithm_two.vhd
+++ Project 2/Algorithms_INCOMPLETE/algorithm_two.vhd
@@ -2,23 +2,20 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity generate_set is
-	port();
-	
 end generate_set;
 
 
 architecture set_gen of generate_set is
 	-- signal decleration
+	     signal c: ads_complex;
+        signal color_index: natural;
+        signal color: std_logic_vector(3 downto 0);
+        signal l: line;
+		  signal p: naural;
 	
 	begin
 
-	procedure generate_set is
-
-        variable c: ads_complex;
-        variable color_index: natural;
-        variable color: std_logic_vector(3 downto 0);
-        file output_file: text open write_mode is PATH;
-        variable l: line;
+	generate_set:process(c, color_index, color, l, p)
 
     begin
 
@@ -29,11 +26,9 @@ architecture set_gen of generate_set is
                     c := ads_cmplx(to_ads_sfixed(3.2 * real(p) / 640.0 - 2.2), to_ads_sfixed(2.2 * real(240 - l) / 480.0));
                     color_index := compute_point(c, iterations);
                     color := vga_color_map(color_index);
-                    writeline(output_file, color(2) & color(1) & color(0) & "00");
                     
                 end if;
             end loop;
         end loop;
-        file_close(fileptr);
-    end procedure;
+    end process;
 end set_gen;

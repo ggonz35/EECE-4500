f13c84ecff381843b04d35976fc3a7a8aa254203
Gabriel Gonzalez <104934278+ggonz35@users.noreply.github.com>
2023-11-13 18:40:59 -0500

    No pulling your hairs out

--- Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
+++ Project 2/Algorithms_INCOMPLETE/algorithm_one.vhd
@@ -2,6 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 use work.ads.all;  -- Assuming ads_complex_pkg is in the work library
 
+-- Move PipelineRecord type declaration here
+type PipelineRecord is record
+    z : ads_complex;
+    c : ads_complex;
+    stage_data : natural;
+    stage_overflow : boolean;
+end record;
+
 entity compute_point is
     port (
         clk : in STD_LOGIC;
@@ -9,33 +17,22 @@ entity compute_point is
         stage_input : in PipelineRecord;
         stage_output : out PipelineRecord
     );
-	 generic (
+    generic (
         threshold: real := 64.0;  -- Set an appropriate value
         iterations: natural := 100;  -- Set an appropriate value
         stage_number: natural := 42   -- Set an appropriate value
     );
-
 end compute_point;
 
-architecture set_gen of generate_set is
+architecture set_gen of compute_point is
     -- signal declaration
     signal z_real_part_temp, z_imaginary_part_temp : real;
     signal z_overflow, c_overflow : boolean;
-    signal stage_data: natural;
-    signal stage_overflow: boolean;
 
     -- Creates a type called color_map_array with a size of maximum iterations
     type color_map_array is array (natural range <>) of ads_complex;
 
-    -- Define the PipelineRecord type
-    type PipelineRecord is record
-        z : ads_complex;
-        c : ads_complex;
-        stage_data : natural;
-        stage_overflow : boolean;
-    end record;
-
-begin
+    begin
     process(clk, rst)
     begin
         if rst = '1' then
@@ -49,12 +46,7 @@ begin
                 (others => (re => to_ads_sfixed(0), im => to_ads_sfixed(0)));
 
             -- Call the procedure with appropriate parameters
-            compute_point(stage_input.c, iterations, my_color_map);
-
-            -- Update stage_output based on your pipeline logic
-            stage_output.stage_data <= stage_input.stage_data when stage_input.stage_overflow else stage_number;
-            stage_output.stage_overflow <= stage_input.stage_overflow or (z_overflow or c_overflow);
-            stage_output.c <= stage_input.c;
+            compute_point(stage_input, my_color_map, stage_output);
 
             -- Compute z^2 + c
             z_real_part_temp <= (stage_input.z.re ** 2) - (stage_input.z.im ** 2) + stage_input.c.re;
@@ -67,6 +59,11 @@ begin
             -- Use temporary values to update z
             stage_output.z.re <= z_real_part_temp;
             stage_output.z.im <= z_imaginary_part_temp;
+
+            -- Update stage_output based on your pipeline logic
+            stage_output.stage_data <= stage_input.stage_data when stage_input.stage_overflow else stage_number;
+            stage_output.stage_overflow <= stage_input.stage_overflow or (z_overflow or c_overflow);
+            stage_output.c <= stage_input.c;
 
         end if;
     end process;

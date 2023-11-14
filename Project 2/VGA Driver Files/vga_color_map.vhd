31223353030764dd85366bc22ec52d08eb955a16
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-11-13 20:08:35 -0500

    DEATH TO AMERICA, AND EDUARDOS COLOR CHOICES

--- Project 2/VGA Driver Files/vga_color_map.vhd
+++ Project 2/VGA Driver Files/vga_color_map.vhd
@@ -5,36 +5,32 @@ use ieee.std_logic_1164.all;
 entity vga_color_map is
 	port(
 	color_index		: in std_logic;
-	color_map_out	: out Array_color_map
+	color_map_out	: in color_map;
 	);
 end vga_color_map;
 
 
 architecture vga_c_map of vga_color_map is
-	--declarative empty atm
-	
 
 	begin
 		color_map 	: process(color_index, color_map_out)
 		begin
-		for i in 0 to 3 loop
 			if color_index = '0' then
-				color_map_out[i].red   <= "0000"; 
-				color_map_out[i].green <= "0000";
-				color_map_out[i].blue  <= "1111";
+				color_map_out.red   <= "0000"; 
+				color_map_out.green <= "0000";
+				color_map_out.blue  <= "1111";
 			elsif color_index = '1' then
-				color_map_out[i].red   <= "0000";
-				color_map_out[i].green <= "0000";
-				color_map_out[i].blue  <= "1111";
+				color_map_out.red   <= "0000";
+				color_map_out.green <= "0000";
+				color_map_out.blue  <= "1111";
 			elsif color_index = '2' then
-				color_map_out[i].red   <= "1111";
-				color_map_out[i].green <= "0000";
-				color_map_out[i].blue  <= "0000";
+				color_map_out.red   <= "1111";
+				color_map_out.green <= "0000";
+				color_map_out.blue  <= "0000";
 			else
-				color_map_out[i].red   <= "0000";
-				color_map_out[i].green <= "0000";
-				color_map_out[i].blue  <= "0000";
+				color_map_out.red   <= "0000";
+				color_map_out.green <= "0000";
+				color_map_out.blue  <= "0000";
 			end if;
-		end loop;
 	end process color_map;
 end vga_c_map;

bda92cd33ccd456dbe5f05f26725cb7b3a6ef7b7
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-11-19 01:33:52 -0500

    Update algorithm_two.vhd

--- Project 2/Algorithms_INCOMPLETE/algorithm_two.vhd
+++ Project 2/Algorithms_INCOMPLETE/algorithm_two.vhd
@@ -1,6 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+use work.ads_complex_pkg.all;
+use work.vga_data.all;
+use work.ads_fixed.all;
+
 entity generate_set is
 end generate_set;
 
@@ -10,12 +14,12 @@ architecture set_gen of generate_set is
 	     signal c: ads_complex;
         signal color_index: natural;
         signal color: std_logic_vector(3 downto 0);
-        signal l: line;
-		  signal p: naural;
+		  signal l: natural;
+		  signal p: natural;
 	
 	begin
 
-	generate_set:process(c, color_index, color, l, p)
+	generate_set:process(c, color_index, color, p)
 
     begin
 
@@ -23,12 +27,14 @@ architecture set_gen of generate_set is
             for p in 0 to 639 loop
 
                 if x_visible(make_coordinate(p, l)) and y_visible(make_coordinate(p, l)) then
-                    c := ads_cmplx(to_ads_sfixed(3.2 * real(p) / 640.0 - 2.2), to_ads_sfixed(2.2 * real(240 - l) / 480.0));
-                    color_index := compute_point(c, iterations);
-                    color := vga_color_map(color_index);
+                    c <= ads_cmplx(to_ads_sfixed(3.2 * real(p) / 640.0 - 2.2), to_ads_sfixed(2.2 * real(240 - l) / 480.0));
+                    color_index <= compute_point(c, iterations);
+                    color <= vga_color_map(color_index);
                     
                 end if;
             end loop;
         end loop;
     end process;
 end set_gen;
+
+

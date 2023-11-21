75f956acbbc796bb672ad4bfbc19bc5476402cdf
E-Alv <147648665+E-Alv@users.noreply.github.com>
2023-11-20 20:17:20 -0500

    ADDED GABES CODE (THIS CODE IS NOT MINE), ATTEMPTING TO TEST PIPELINE STUFF

--- Project 2/Algorithms_INCOMPLETE/pipeline_stage.vhd
+++ Project 2/Algorithms_INCOMPLETE/pipeline_stage.vhd
@@ -20,6 +20,23 @@ architecture behavior of pipeline_stage is
     signal stage_output: complex_record;
 begin
 
-    stage_output.stage_data <= stage_input.stage_data when
+    --perform computations and complete assignments
+   -- Calculate stage_data and stage_overflow based on the rules
+   stage_output.stage_data <= stage_input.stage_data when stage_input.stage_overflow else stage_number;
+
+   stage_output.stage_overflow <= stage_input.stage_overflow or (z_overflow or c_overflow); -- Assign c output as it is
+   stage_output.c <= stage_input.c;   -- Compute z^2 + c
+
+   z_real_part_temp <= (stage_input.z.real_part ** 2) - (stage_input.z.imaginary_part ** 2) + stage_input.c.real_part;
+
+   z_imaginary_part_temp <= (2.0 * stage_input.z.real_part * stage_input.z.imaginary_part) + stage_input.c.imaginary_part; 
+     -- Calculate overflow flags for z and c
+   z_overflow <= (abs(z_real_part_temp) > threshold) or (abs(z_imaginary_part_temp) > threshold);
+
+   c_overflow <= (abs(stage_input.c.real_part) > threshold) or (abs(stage_input.c.imaginary_part) > threshold);   -- Use temporary values to update z
+   
+   stage_output.z.real_part <= z_real_part_temp;
+   
+   stage_output.z.imaginary_part <= z_imaginary_part_temp;
 
 end behavior;

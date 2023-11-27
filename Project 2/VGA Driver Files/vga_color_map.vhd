457562b1e49391797076cb5491ef2022ee5474d9
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-11-27 15:13:49 -0500

    Update vga_color_map.vhd

--- Project 2/VGA Driver Files/vga_color_map.vhd
+++ Project 2/VGA Driver Files/vga_color_map.vhd
@@ -14,21 +14,21 @@ end vga_color_map;
 architecture vga_c_map of vga_color_map is
 	type color_map_array_type is array(natural range<>) of color_map;
 	constant my_color_map: color_map_array_type := (
-			( red => "0000", green => "1111", blue => "0000" ),
-			( red => "0001", green => "1110", blue => "0000" ),
-			( red => "0010", green => "1101", blue => "0000" ),
-			( red => "0011", green => "1100", blue => "0000" ),
-			( red => "0100", green => "1011", blue => "0000" ),
-			( red => "0101", green => "1010", blue => "0000" ),
-			( red => "0110", green => "1001", blue => "0000" ),
-			( red => "0111", green => "1000", blue => "0000" ),
-			( red => "1000", green => "0111", blue => "0000" ),
-			( red => "1001", green => "0110", blue => "0000" ),
-			( red => "1010", green => "0101", blue => "0000" ),
-			( red => "1011", green => "0100", blue => "0000" ),
-			( red => "1100", green => "0011", blue => "0000" ),
-			( red => "1101", green => "0010", blue => "0000" ),
-			( red => "1110", green => "0001", blue => "0000" ),
+			( red => "0000", green => "0000", blue => "1111" ),
+			( red => "0001", green => "0000", blue => "1110" ),
+			( red => "0010", green => "0000", blue => "1101" ),
+			( red => "0011", green => "0000", blue => "1100" ),
+			( red => "0100", green => "0000", blue => "1011" ),
+			( red => "0101", green => "0000", blue => "1010" ),
+			( red => "0110", green => "0000", blue => "1001" ),
+			( red => "0111", green => "0000", blue => "1000" ),
+			( red => "1000", green => "0000", blue => "0111" ),
+			( red => "1001", green => "0000", blue => "0110" ),
+			( red => "1010", green => "0000", blue => "0101" ),
+			( red => "1011", green => "0000", blue => "0100" ),
+			( red => "1100", green => "0000", blue => "0011" ),
+			( red => "1101", green => "0000", blue => "0010" ),
+			( red => "1110", green => "0000", blue => "0001" ),
 			( red => "1111", green => "0000", blue => "0000" )
 		);
 

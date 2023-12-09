2af822c6ac9f283cd66cd360a80a507df2f5f58a
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-12-09 18:58:51 -0500

    This bad boi can hold so much pain

--- Project 4/Sync_parts/sync_stage.vhd
+++ Project 4/Sync_parts/sync_stage.vhd
@@ -20,6 +20,11 @@ architecture sync of sync_stage is
 			input_width: positive := 16
 		);
 		port(
+			clk			:in	std_logic;
+			rst			:in	std_logic;
+			in_data		:in	std_logic_vector(input_width - 1 downto 0);
+		
+			out_data		:out	std_logic_vector(input_width - 1 downto 0)
 		);
 	end component sync_ff;
 	
@@ -28,6 +33,8 @@ architecture sync of sync_stage is
 			input_width: positive := 16
 		);
 		port(
+			bin_in: in std_logic_vector(input_width - 1 downto 0);
+			gray_out: out std_logic_vector(input_width - 1 downto 0)
 		);	
 	end component binary_to_gray;
 	
@@ -36,10 +43,45 @@ architecture sync of sync_stage is
 			input_width: positive := 16
 		);
 		port(
+			gray_in: in std_logic_vector(input_width - 1 downto 0);
+			bin_out: out std_logic_vector(input_width - 1 downto 0)
 		);	
 	end component gray_to_binary
 begin
 
+	b2g :		 binary_to_gray
+		generic map(
+			input_width	=> input_width
+		);
+		port map(
+			bin_in 		=> bin_in,
+			gray_out 	=> gray_in
+		);
+		
+	g2b :		 gray_to_binary
+		generic map(
+			input_width	=> input_width
+		);
+		port map(
+			gray_out		=> gray_in,
+			bin_out 		=> bin_out
+		);
+		
+	ff : 		sync_ff
+		generic map(
+			input_width	=> input_width
+		);
+		port map(
+			clk			=> clk,
+			rst			=> rst,
+			gray_out		=> gray_in,
+		
+			gray_out		=> gray_in
+		);
+		
+		
+	flippity_floppity(clk,rst);
+		
 
 
 end architecture sync;
